ccff9a2fb2800d9f794d9762901fa3019b8d66f8
Clement Escoffier <clement@apache.org>
2025-09-23 16:26:50 +0200

    Fix MCP test

--- section-1/step-08-mcp-server/src/test/java/dev/langchain4j/quarkus/workshop/WeatherServiceTest.java
+++ section-1/step-08-mcp-server/src/test/java/dev/langchain4j/quarkus/workshop/WeatherServiceTest.java
@@ -1,7 +1,10 @@
 package dev.langchain4j.quarkus.workshop;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
@@ -15,10 +18,11 @@ public class WeatherServiceTest {
     public void testGetForecast() {
 
         // Use RestAssured to test the endpoint
-        given()
-          .when().get("weather?latitude=52.52&longitude=13.41")
-          .then()
-             .statusCode(200)
-             .body(is("{\"temperature\": \"20\", \"precipitation\": \"0\", \"precipitation_probability\": \"0\"}"));
+        var s = given()
+                .when().get("weather?latitude=52.52&longitude=13.41")
+                .then()
+                .statusCode(200)
+                .extract().body().asString();
+        assertThat(s).contains("temperature_2m", "rain", "precipitation");
     }
 }
