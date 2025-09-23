3e471cdf96711225dd4cc7c4600ea2699b9d6830
Georgios Andrianakis <geoand@gmail.com>
2025-09-23 18:07:16 +0300

    Simplify code in Weather MCP step

--- section-1/step-08-mcp-server/src/main/java/dev/langchain4j/quarkus/workshop/WeatherClient.java
+++ section-1/step-08-mcp-server/src/main/java/dev/langchain4j/quarkus/workshop/WeatherClient.java
@@ -4,16 +4,17 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/v1/forecast")
 @RegisterRestClient(configKey="weatherclient")
 public interface WeatherClient {
 
     @GET
-    public String getForecast(
-            @QueryParam("latitude") double latitude,
-            @QueryParam("longitude") double longitude,
-            @QueryParam("forecastdays") int forecastDays,
-            @QueryParam("hourly") String hourly
+    String getForecast(
+            @RestQuery double latitude,
+            @RestQuery double longitude,
+            @RestQuery int forecastDays,
+            @RestQuery String hourly
     );
 }
