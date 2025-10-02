60c0d6f4b88adb40139246fb35dd349fafe3b5b8
Kevin Dubois <kevin.dubois@gmail.com>
2025-10-02 09:21:55 +0200

    simplify import statements

--- section-2/step-01/src/main/resources/import.sql
+++ section-2/step-01/src/main/resources/import.sql
@@ -1,24 +1,20 @@
+CREATE SEQUENCE IF NOT EXISTS car_info_id_seq;
+
 CREATE TABLE IF NOT EXISTS car_info (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY DEFAULT nextval('car_info_id_seq'),
     make VARCHAR(255) NOT NULL,
     model VARCHAR(255) NOT NULL,
     year INT NOT NULL,
     status VARCHAR(20) NOT NULL
 );
 
-INSERT INTO car_info (id, make, model, year, status) VALUES (1, 'Toyota', 'Corolla', 2020, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (2, 'Honda', 'Civic', 2019, 'RENTED');
-INSERT INTO car_info (id, make, model, year, status) VALUES (3, 'Ford', 'F-150', 2021, 'AT_CAR_WASH');
-INSERT INTO car_info (id, make, model, year, status) VALUES (4, 'Chevrolet', 'Malibu', 2018, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (5, 'BMW', 'X5', 2022, 'AT_CAR_WASH');
-INSERT INTO car_info (id, make, model, year, status) VALUES (6, 'Mercedes-Benz', 'C-Class', 2020, 'RENTED');
-INSERT INTO car_info (id, make, model, year, status) VALUES (7, 'Audi', 'A4', 2021, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (8, 'Nissan', 'Altima', 2017, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (9, 'Toyota', 'Camry', 2019, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (10, 'Honda', 'Accord', 2020, 'RENTED');
-INSERT INTO car_info (id, make, model, year, status) VALUES (11, 'Ford', 'Mustang', 2022, 'AT_CAR_WASH');
-INSERT INTO car_info (id, make, model, year, status) VALUES (12, 'Chevrolet', 'Silverado', 2021, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (13, 'BMW', '3 Series', 2020, 'AT_CAR_WASH');
-INSERT INTO car_info (id, make, model, year, status) VALUES (14, 'Mercedes-Benz', 'E-Class', 2023, 'AVAILABLE');
-INSERT INTO car_info (id, make, model, year, status) VALUES (15, 'Audi', 'Q5', 2022, 'RENTED');
-
+INSERT INTO car_info (id, make, model, year, status) VALUES
+    (nextval('car_info_id_seq'), 'Toyota', 'Corolla', 2020, 'AVAILABLE'),
+    (nextval('car_info_id_seq'), 'Honda', 'Civic', 2019, 'RENTED'),
+    (nextval('car_info_id_seq'), 'Ford', 'F-150', 2021, 'AT_CAR_WASH'),
+    (nextval('car_info_id_seq'), 'Chevrolet', 'Malibu', 2018, 'AVAILABLE'),
+    (nextval('car_info_id_seq'), 'BMW', 'X5', 2022, 'AT_CAR_WASH'),
+    (nextval('car_info_id_seq'), 'Mercedes-Benz', 'C-Class', 2020, 'RENTED'),
+    (nextval('car_info_id_seq'), 'Audi', 'A4', 2021, 'AVAILABLE'),
+    (nextval('car_info_id_seq'), 'Nissan', 'Altima', 2017, 'RENTED'),
+    (nextval('car_info_id_seq'), 'Audi', 'Q4', 2022, 'RENTED');
